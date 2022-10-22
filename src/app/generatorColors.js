b74d584bee1ec858c98407ef093327aa6857337f
Makavetskaya Tatsiana <makavetskaya.t@gmail.com>
2022-10-22 18:42:37 +0300

    add function copy

--- src/app/generatorColors.js
+++ src/app/generatorColors.js
@@ -2,6 +2,7 @@ const colorBlock = document.querySelectorAll('.color')
 const cols = document.querySelectorAll('.col')
 const hexCode = document.querySelectorAll('p')
 const wrapper = document.querySelector('.wrapper')
+const copy = document.querySelectorAll('.copied')
 
 console.log(cols)
 
@@ -37,14 +38,45 @@ const upDateColors = ()=>{
 }
 upDateColors()
 
-///// changes the color of only the clicked element
+///// changes the color of only the clicked element and copy hexCode
 
-document.addEventListener('click', e=>{
-    for( let i = 0; i<colorBlock.length; i++){
+document.addEventListener('click', e =>{
+    let currElem = e.target
+    for( let i = 0; i < colorBlock.length; i++){
         if(e.target === colorBlock[i]){
             colorBlock[i].style.backgroundColor = generationColors()
             hexCode[i].textContent =  generationColors()
         }
     }
+    if (currElem.classList.contains('code')){
+        return copyhexCode(currElem.textContent)
+       
+    }
 })
 
+
+const copyhexCode = (text) =>{
+ return navigator.clipboard.writeText(text)
+
+}
+copyhexCode()
+
+
+
+document.addEventListener('mouseover',e=>{
+    for( let i = 0; i <hexCode.length; i++){
+        if(e.target === hexCode[i]){
+            copy[i].classList.remove('hide')
+        }else{
+            copy[i].classList.add('hide')
+        }
+    }
+})
+document.addEventListener('click',e=>{
+    for( let i = 0; i <hexCode.length; i++){
+        if(e.target === hexCode[i]){
+            copy[i].textContent = 'Copied'
+        } 
+        
+    }
+})
